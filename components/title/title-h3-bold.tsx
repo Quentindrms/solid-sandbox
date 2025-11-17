0d2736af61b431e492e23e04e0b83cd3464b05dc
Quentin Derimais <quentin.derimais@gmail.com>
2025-11-17 11:34:54 +0100

    Création d'une page avec fetch de données

--- components/title/title-h3-bold.tsx
+++ components/title/title-h3-bold.tsx
@@ -7,7 +7,7 @@ type TitleH3Props = {
 
 export function TitleH3({text, color}:TitleH3Props){
 
-    return(<h3 class={`font-bold text-${color}-500 text-5xl m-1 p-1`}>{text}</h3>)
+    return(<h3 class={`font-bold text-${color}-500 text-3xl m-5 p-1`}>{text}</h3>)
 }
 
 export default TitleH3;
