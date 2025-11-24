d7d56a85749b8061824a7ca080f0fbe730de908c
Quentin Derimais <quentin.derimais@gmail.com>
2025-11-24 13:37:26 +0100

    Migration des fichiers et suppresion de Express pour passage sur Vike

--- database/Prisma.ts
+++ database/Prisma.ts
@@ -7,7 +7,7 @@ class Prisma {
     private static instance: PrismaClient;
 
     public static getInstance(): PrismaClient {
-        const connectionString = `${process.env.DATABASE_URL}` || undefined;
+        const connectionString = `${import.meta.env.DATABASE_URL}` || undefined;
         if (!Prisma.instance) {
             const adapter = new PrismaPg({ connectionString })
             Prisma.instance = new PrismaClient({
--- server/controllers/GlobalController.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "../libs/Controller";
-
-export class GlobalController extends Controller{
-
-    hello(){
-        this.response.send('Hello world');
-    }
-
-}
